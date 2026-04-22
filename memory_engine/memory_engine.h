a9a47c67672130842214040b302ea7ff40df75c7
zyntax-zx <68755927+zyntax-zx@users.noreply.github.com>
2026-04-21 22:37:00 -0500

    Delete memory_engine directory

--- memory_engine/memory_engine.h
+++
@@ -1,8 +0,0 @@
-// memory_engine/memory_engine.h
-#pragma once
-#include <cstdint>     // ← Esta línea faltaba
-
-namespace memory_engine {
-    void init();
-    bool write_float(uint64_t addr, float value);
-}
